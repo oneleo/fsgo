a7bda5159fa99d313e1379efe33439ab73b82679
oneleo <oneleo760823@gmail.com>
2020-12-31 15:12:20 +0800

    Update module.go

--- fileserver/module.go
+++ fileserver/module.go
@@ -4,6 +4,7 @@ import (
 	"archive/zip"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -34,6 +35,9 @@ func (s *Module) Compress(w http.ResponseWriter, r *http.Request) {
 		zipWriter := zip.NewWriter(w)
 		defer zipWriter.Close()
 
+		// 收集錯誤用，因 filepath.Walk 收到 err 會停止遍歷。
+		var zipErrors []error
+
 		filepath.Walk(zipPath, func(relatePath string, info os.FileInfo, err error) error {
 			subPath := strings.TrimPrefix(relatePath, zipPath)
 			// 根目錄為空，故要排除在壓縮檔之外
@@ -42,7 +46,8 @@ func (s *Module) Compress(w http.ResponseWriter, r *http.Request) {
 			}
 			hdr, err := zip.FileInfoHeader(info)
 			if err != nil {
-				return err
+				zipErrors = append(zipErrors, err)
+				return nil
 			}
 			// subPath 開頭為 / 或 \，要將之刪除，否則無法加入至壓縮檔。
 			hdr.Name = strings.TrimLeft(subPath, `/\`)
@@ -60,7 +65,8 @@ func (s *Module) Compress(w http.ResponseWriter, r *http.Request) {
 			// 在壓縮檔內建置對映空殼檔（尚無對映的資料）
 			zw, err := zipWriter.CreateHeader(hdr)
 			if err != nil {
-				return err
+				zipErrors = append(zipErrors, err)
+				return nil
 			}
 			// 若此檔是捷徑，則取得它原來的檔名資訊
 			if info.Mode()&os.ModeSymlink == os.ModeSymlink { // 是捷徑
@@ -68,7 +74,8 @@ func (s *Module) Compress(w http.ResponseWriter, r *http.Request) {
 				var symlink string
 				symlink, err = os.Readlink(relatePath)
 				if err != nil {
-					return err
+					zipErrors = append(zipErrors, err)
+					return nil
 				}
 				zw.Write([]byte(symlink))
 				// _, err = io.Copy(zw, ioutil.NopCloser(bytes.NewBuffer([]byte(symlink))))
@@ -85,7 +92,8 @@ func (s *Module) Compress(w http.ResponseWriter, r *http.Request) {
 				// 將此檔的資料寫入對映空殼檔後壓縮
 				data, err := ioutil.ReadFile(relatePath)
 				if err != nil {
-					return err
+					zipErrors = append(zipErrors, err)
+					return nil
 				}
 				zw.Write(data)
 			} else if info.IsDir() { // 是資料夾
@@ -98,6 +106,9 @@ func (s *Module) Compress(w http.ResponseWriter, r *http.Request) {
 			}
 			return nil
 		})
+		for _, e := range zipErrors {
+			log.Println("zip err: ", e)
+		}
 		return
 	}
 	return
